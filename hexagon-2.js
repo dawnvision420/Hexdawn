e1e25ba2e519f0d266fdc7d8455ce501da3e2599
dawnvision420 <alali.mufarrah@gmail.com>
2017-02-23 06:33:03 -0800

    this is draft for the website

---
+++ hexagon-2.js
@@ -0,0 +1,9 @@
+function GetClock(){
+	d = new Date();
+	nhour  = d.getHours();
+	nmin   = d.getMinutes();
+	if(nmin <= 9){nmin="0"+nmin}
+	document.getElementById('clockbox').innerHTML=""+nhour+" "+nmin+"";
+	setTimeout("GetClock()", 1000);
+}
+onload=GetClock;
